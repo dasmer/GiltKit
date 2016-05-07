36628801a4b3b1eb079bb2c279dc117d71490d63
Dasmer Singh <dasmer.singh@venmo.com>
2016-05-07 13:37:35 -0400

    Add OSX Scheme

--- GiltKit/GiltKit.h
+++ GiltKit/GiltKit.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double GiltKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char GiltKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <GiltKit/PublicHeader.h>
-
-
